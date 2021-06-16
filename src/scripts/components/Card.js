ffd5b3ffb6a258ef9b08dd0d823549b1d1afc45d
Zhanna Anikanova <whiteredfoxx@gmail.com>
2021-06-17 02:14:15 +0300

    feat:форматирование цены

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -6,8 +6,8 @@ export default class Card {
         this._title = cardData.title;
         this._address = cardData.address;
         this._type = cardData.type;
-        this._price = cardData.price
-        this._id = cardData.id
+        this._price = cardData.price;
+        this._id = cardData.id;
         this._cardTypes = {
             IndependentLiving: "card__label_type_independ",
             SupportAvailable: "card__label_type_support"
@@ -18,11 +18,18 @@ export default class Card {
         return document.querySelector(this._template).content.cloneNode(true)
     }
 
+    _formatPrice() {
+        this._price = Number.prototype.toFixed.call(parseFloat(this._price) || 0, 2)
+        this._price = this._price.replace(/(\D)/g, ",")
+        this._price = this._price.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ");
+        return '£ ' + this._price;
+    }
+
     createCard() {
         this._card = this._createCardTeml();
         this._card.querySelector('.card__title').textContent = this._title;
         this._card.querySelector('.card__text_type_address').textContent = this._address;
-        this._card.querySelector('.card__text-price').textContent ='£ ' + this._price;
+        this._card.querySelector('.card__text-price').textContent = this._formatPrice();
         this._card.querySelector('.card__img').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
         this._card.querySelector('.card__img').alt = 'arkhyz';
         this._card.querySelector('.card__label-caption').textContent = this._type
